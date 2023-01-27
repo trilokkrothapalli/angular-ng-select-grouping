4996f8fa1fcf3bac8a41ff34ef91fcbc1fd1f006
trilokkrothapalli <37873229+trilokkrothapalli@users.noreply.github.com>
2023-01-27 09:13:41 +0530

    2 files modified

--- src/group-default-example.component.ts
+++ src/group-default-example.component.ts
@@ -7,21 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupDefaultExampleComponent implements OnInit {
 
-    selectedAccount = 'Adam';
+    
     accounts = [
-        { name: 'Adam', email: 'adam@email.com', age: 12, country: 'United States', child: { state: 'Active' } },
-        { name: 'Homer', email: 'homer@email.com', age: 47, country: '', child: { state: 'Active' } },
-        { name: 'Samantha', email: 'samantha@email.com', age: 30, country: 'United States', child: { state: 'Active' } },
-        { name: 'Amalie', email: 'amalie@email.com', age: 12, country: 'Argentina', child: { state: 'Active' } },
-        { name: 'Estefanía', email: 'estefania@email.com', age: 21, country: 'Argentina', child: { state: 'Active' } },
-        { name: 'Adrian', email: 'adrian@email.com', age: 21, country: 'Ecuador', child: { state: 'Active' } },
-        { name: 'Wladimir', email: 'wladimir@email.com', age: 30, country: 'Ecuador', child: { state: 'Inactive' } },
-        { name: 'Natasha', email: 'natasha@email.com', age: 54, country: 'Ecuador', child: { state: 'Inactive' } },
-        { name: 'Nicole', email: 'nicole@email.com', age: 43, country: 'Colombia', child: { state: 'Inactive' } },
-        { name: 'Michael', email: 'michael@email.com', age: 15, country: 'Colombia', child: { state: 'Inactive' } },
-        { name: 'Nicolás', email: 'nicole@email.com', age: 43, country: 'Colombia', child: { state: 'Inactive' } }
+        {
+            viewId: 0,
+            viewGuid: "00000000-0000-0000-0000-000000000000",
+            viewName: "No saved views",
+            viewType: "Financial",
+            viewGroup: "YOUR VIEWS",
+            disabled: true
+        },
+        {
+            viewId: -1,
+            viewGuid: "00000000-0000-0000-0000-000000000000",
+            viewName: "Financial View",
+            viewType: "Financial",
+            viewGroup: "QUICK VIEWS",
+            disabled: false
+        }
     ];
-
+    selectedAccount = this.accounts.some(x => x.viewId ==0);
     constructor() {
     }
 
